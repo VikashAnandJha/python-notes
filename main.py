68e44ea449b54eb5e5340617b78aa775fcd5a94d
VikashAnandJha <mailtovikashjha@gmail.com>
2023-08-29 11:42:24 +0530

    google search

--- main.py
+++ main.py
@@ -1,41 +1,16 @@
-import requests
-import time
-import os
-import random
+from googlesearch import search
 
-from bs4 import BeautifulSoup
+query = "goldratetoday.shop"  # Replace with your search query
+website = "goldratetoday.shop"
+# Perform the search and retrieve the URLs of the search results
+# num is the number of results, stop is the total results to retrieve
+search_results = search(query, num_results=50, advanced=True)
 
-
-def getScore():
-    # URL of the cricket World Cup page
-    random_integer = random.randint(1, 1000000000000)
-
-    url = "https://www.espncricinfo.com/series/new-zealand-a-in-australia-2023-1392778/australia-a-vs-new-zealand-a-1st-unofficial-test-1392782/live-cricket-score?random=" + \
-        str(random_integer)
-
-    # Send an HTTP GET request to the URL
-    response = requests.get(url)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the HTML content
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Find all match elements
-        data_21_4_ov = soup.find(
-            'span', class_='ds-text-compact-s').text.strip()
-        score_147 = soup.find('strong', class_='ds-text-typo-mid3').text
-        score_81_1 = soup.find('strong', class_='').text
-
-        print("Data  :", data_21_4_ov)
-        print("Score :", score_147)
-        print("Score :", score_81_1)
-
-    else:
-        print("Failed to retrieve the page. Status code:", response.status_code)
-
-
-while True:
-    os.system('clear')
-    getScore()
-    time.sleep(5)  # Sleep for 5 seconds
+# Print the URLs of the search results
+i = 0
+for result in search_results:
+    i += 1
+    print(i, result.title, result.url)
+    if (website in result.url):
+        print("====== found =======")
+        break
