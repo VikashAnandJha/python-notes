d9d4dc13522530cd7e12d6563f35b4a96e9e63d3
VikashAnandJha <mailtovikashjha@gmail.com>
2023-08-29 12:04:56 +0530

    selenium

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from googlesearch import search
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-query = "goldratetoday.shop"  # Replace with your search query
-website = "goldratetoday.shop"
-# Perform the search and retrieve the URLs of the search results
-# num is the number of results, stop is the total results to retrieve
-search_results = search(query, num_results=50, advanced=True)
+# Create a new instance of the Firefox driver (you can use other drivers like Chrome as well)
+driver = webdriver.Chrome()
 
-# Print the URLs of the search results
-i = 0
-for result in search_results:
-    i += 1
-    print(i, result.title, result.url)
-    if (website in result.url):
-        print("====== found =======")
-        break
+# Navigate to the website
+driver.get("https://lowcars.co.in")
+
+try:
+    # Find and click the login button
+    login_button = driver.find_element(By.LINK_TEXT, "Contact")
+    login_button.click()
+
+    # Wait for a few seconds to allow the page to load after clicking the button
+    driver.implicitly_wait(5)  # You can adjust the waiting time as needed
+    login_button = driver.find_element(By.LINK_TEXT, "Offers")
+    driver.implicitly_wait(5)
+    login_button.click()
+    # Print the current page's HTML content
+    print(driver.page_source)
+
+except Exception as e:
+    print("An error occurred:", e)
+
+# Close the browser
+# driver.quit()
